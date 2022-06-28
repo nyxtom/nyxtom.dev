99d474d545d6b71864fe6335dc3e83b6b76bfe50
Thomas Holloway <nyxtom@gmail.com>
2022-06-28 13:52:06 -0500

    Add tracing support with default subscriber, env and fmt with tide tracing

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@ mod registry;
 mod routes;
 
 use tide::utils::After;
+use tide_tracing::TraceMiddleware;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[async_std::main]
 async fn main() -> std::io::Result<()> {
     let mut app = tide::new();
-    env_logger::init();
+    // env_logger::init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     // serve static files
     app.at("/static").serve_dir("client/dist")?;
     app.at("/assets").serve_dir("posts/assets")?;
     app.at("/favicon.ico").serve_file("favicon.ico")?;
 
-    app.with(tide::log::LogMiddleware::new());
+    // app.with(tide::log::LogMiddleware::new());
+    app.with(TraceMiddleware::new());
     app.with(After(errors::error_handler));
     routes::configure(&mut app);
 
